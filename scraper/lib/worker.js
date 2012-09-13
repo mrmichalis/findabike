7d9b4983bbab4d44af72f086d04781aa82e98ea7
bcoe <=>
2012-09-12 23:03:51 -0700

    Updated worker.

--- scraper/lib/worker.js
+++ scraper/lib/worker.js
@@ -6,37 +6,54 @@ function Worker(opts) {
 		beanstalkPort: '11300',
 		beanstalkHost: '127.0.0.1'
 	});
-	this._subscribeToEvents();
+	var _this = this;
+	client.connect(this.beanstalkHost + ':' + this.beanstalkPort, function(err, connection) {
+		_this.connection = connection;
+		_this._subscribeToEvents();
+	});
 }
 
-Worker.WORK_QUEUE = 'crawls';
+Worker.CHECK_POST_JOB = 'check_post_job';
+Worker.PARSE_LISTING_JOB = 'parse_listing_job';
 
 Worker.prototype._subscribeToEvents = function() {
 	var _this = this;
 
-	client.connect(this.beanstalkHost + ':' + this.beanstalkPort, function(err, connection) {
+	_this.connection.reserve(function(err, id, jobJSON) {
+		var job = JSON.parse(jobJSON);
 		
-		_this.connection = connection;
+		if (job.email) _this.crawl(job);
 		
-		_this.connection.reserve(function(err, id, jobJSON) {
-			var job = JSON.parse(jobJSON);
-			
-			if (job.email) _this.crawl(job.email);
-			
-			_this._rescheduleWork(id, job);
-		});
+		_this._rescheduleWork(id, job);
 	});
 };
 
 Worker.prototype._rescheduleWork = function(id, job) {
-	
-	this.connection.destroy(id, function(err) {
-		console.log('destroyed job ' + id)
-	});
+	var _this = this;
+	if (job.recurring) {
+		this.connection.put(1, 3, 120, JSON.stringify(job), function() {
+			_this.connection.destroy(id, function(err) {
+				console.log('destroyed ' + job.type + ' job ' + id)
+				_this._subscribeToEvents();
+			});
+		});
+	} else {
+		this.connection.destroy(id, function(err) {
+			console.log('destroyed ' + job.type + ' job ' + id)
+			_this._subscribeToEvents();
+		});
+	}
 };
 
-Worker.prototype.crawl = function(email) {
-	
+Worker.prototype.crawl = function(job) {
+	// CHECK JOB DO TWO TYPES OF WORK HERE.
+	/*
+	conn.put(0, 0, 1, JSON.stringify({
+		email: 'foo@example.com',
+		type: 'yo',
+		recurring: true
+	}), function() {});
+	*/
 };
 
 exports.Worker = Worker;
